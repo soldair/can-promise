982ec405bfa5b570636532df070be59dd6a3df8d
Ryan Day <soldair@gmail.com>
2018-11-02 14:41:17 -0700

    fix: crash undefined global in some versions of react native
    fixes https://github.com/soldair/node-qrcode/issues/157

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 
+// this seems to fail in some versions of react-native
 var G = require('window-or-global')
 
 module.exports = function() {
+  if(!G) G = {}
+  if(typeof Promise !== 'undefined'){
+    G.Promise = Promise
+  }
   return (
     typeof G.Promise === 'function' &&
     typeof G.Promise.prototype.then === 'function'
